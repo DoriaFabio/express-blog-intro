807daf2b12c7cca0545db23e55cff3fe48073f6e
DoriaFabio <doriafabio2001@gmail.com>
2024-12-02 21:00:52 +0100

    create array posts

---
+++ data/post.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        titolo: "Ciambellone",
+        contenuto: "Descrizione del ciambellone",
+        immagine: "../public/images/ciambellone.jpeg",
+        tags: ["ciambellone", "dolci", "cucina"]
+    },
+    {
+        titolo: "Cracker con barbabietola",
+        contenuto: "Descrizione cracker alla barbabietola",
+        immagine: "../public/images/cracker_barbabietola.jpeg",
+        tags: ["cracker", "barbabietola", "spuntino"]
+    },
+    {
+        titolo: "Pane fritto dolce",
+        contenuto: "Descrizione del pane fritto dolce",
+        immagine: "../public/images/pane_fritto_dolce.jpeg",
+        tags: ["pane fritto", "dolci", "pane"]
+    },
+    {
+        titolo: "Pasta con barbabietola",
+        contenuto: "Descrizione della pasta con la barbabietola",
+        immagine: "../public/images/pasta_barbabietola.jpeg",
+        tags: ["pasta", "barbabietola", "farina"]
+    },
+    {
+        titolo: "Torta paesana",
+        contenuto: "Descrizione della torta paesana",
+        immagine: "../public/images/torta_paesana.jpeg",
+        tags: ["torta", "dolci", "paesana"]
+    }
+];
+
+console.log(posts);
